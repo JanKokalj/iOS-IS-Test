bb448445d70e2e724864f1bc52b5617b40992adb
Jan <mrkokalj@gmail.com>
2022-01-27 19:56:00 +0100

    Add project

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TestIS' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TestIS
+  pod 'IronSourceSDK','7.1.14.0'
+
+end
---
+++ TestIS/TestISApp.swift
@@ -0,0 +1,20 @@
+//
+//  TestISApp.swift
+//  TestIS
+//
+//  Created by Jan on 27/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestISApp: App {
+    var body: some Scene {
+        
+        //IronSource.initWithAppKey("kAPPKEY")
+        
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
